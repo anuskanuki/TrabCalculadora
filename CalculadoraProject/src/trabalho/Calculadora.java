0197def55ae7d6be820a06715f2a5c9ca3d3519a
Gabriel Andrade dos Santos <gabrielusa11@hotmail.com>
2020-04-12 22:15:15 -0300

    Correção para números com decimais

--- CalculadoraProject/src/trabalho/Calculadora.java
+++ CalculadoraProject/src/trabalho/Calculadora.java
@@ -11,7 +11,7 @@ public class Calculadora {
         char[] tokens = exp.toCharArray();
 
         // Cria uma pilha vetor vazia
-        Pilha<Integer> pilha = new PilhaVetor<>(exp.length());
+        Pilha<Double> pilha = new PilhaVetor<>(exp.length());
         // Pilha<Integer> stack = new PilhaLista<>(exp.length());
 
         for (int i = 0; i < tokens.length; i++) {
@@ -22,18 +22,18 @@ public class Calculadora {
 
             // Se for um número, adiciona na pilhaVetor
             if (tokens[i] >= '0' && tokens[i] <= '9') {
-                StringBuffer sbuf = new StringBuffer();
+                StringBuilder sbud = new StringBuilder();
                 // Se for um número com mais de uma casa
-                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9') {
-                    sbuf.append(tokens[i++]);
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.') {
+                    sbud.append(tokens[i++]);
                 }
-                pilha.push(Integer.parseInt(sbuf.toString()));
+                pilha.push(Double.parseDouble(sbud.toString()));
             } // Se for um operador
             else {
 
                 // Tirar dois valores
-                int x = pilha.pop();
-                int y = pilha.pop();
+                double x = pilha.pop();
+                double y = pilha.pop();
 
                 // Faz a operação, e adiciona na pilhaVetor
                 if (exp.charAt(i) == '+') {
@@ -57,7 +57,7 @@ public class Calculadora {
         char[] tokens = exp.toCharArray();
 
         // Cria uma pilha vetor vazia
-        Pilha<Integer> stack = new PilhaLista<>();
+        Pilha<Double> stack = new PilhaLista<>();
         // Pilha<Integer> stack = new PilhaLista<>(exp.length());
 
         for (int i = 0; i < tokens.length; i++) {
@@ -68,18 +68,18 @@ public class Calculadora {
 
             // Se for um número, adiciona na pilhaVetor
             if (tokens[i] >= '0' && tokens[i] <= '9') {
-                StringBuffer sbuf = new StringBuffer();
+                StringBuilder sbud = new StringBuilder();
                 // Se for um número com mais de uma casa
-                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9') {
-                    sbuf.append(tokens[i++]);
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.') {
+                    sbud.append(tokens[i++]);
                 }
-                stack.push(Integer.parseInt(sbuf.toString()));
+                stack.push(Double.parseDouble(sbud.toString()));
             } // Se for um operador
             else {
 
                 // Tirar dois valores
-                int x = stack.pop();
-                int y = stack.pop();
+                double x = stack.pop();
+                double y = stack.pop();
 
                 // Faz a operação, e adiciona na pilhaVetor
                 if (exp.charAt(i) == '+') {
@@ -97,20 +97,20 @@ public class Calculadora {
         return stack.pop();
     }
 
-//    public static void main(String[] args) throws Exception {
-////		Expressão (1-2)*(4+5) = -9
-//        String exp1 = "1 2 - 4 5 + *";
-//
-////		Expressão ((23+12)/7)*(5+(3-12)) = -20
-//        String exp2 = "23 12 + 7 / 3 12 - 5 + *";
-//
-////		Expressão  5 + (1 + 2) × 4 – 3 = 14
-//        String exp3 = "5 1 2 + 4 * + 3 -";
-//
-////		Expressão (5.0 + 3) * 2 / (4 - 6) = -8.0    // HELP !!!!!!!!!!!
-////		String exp4 = "5.0 3 + 2 * 4 6 - /";
-//        System.out.println("PilhaVetor = " + Vetor(exp1) + " " + Vetor(exp2) + "  " + Vetor(exp3)/*+Vetor(exp4)*/);
-//        System.out.println("PilhaLista = " + Lista(exp1) + " " + Lista(exp2) + "  " + Lista(exp3)/*+Lista(exp4)*/);
-//    }
+    public static void main(String[] args) throws Exception {
+//		Expressão (1-2)*(4+5) = -9
+        String exp1 = "1 2 - 4 5 + *";
+
+//		Expressão ((23+12)/7)*(5+(3-12)) = -20
+        String exp2 = "23 12 + 7 / 3 12 - 5 + *";
+
+//		Expressão  5 + (1 + 2) × 4 – 3 = 14
+        String exp3 = "5 1 2 + 4 * + 3 -";
+
+//		Expressão (5.0 + 3) * 2 / (4 - 6) = -8.0    // HELP !!!!!!!!!!!
+        String exp4 = "5.0 3 + 2 * 4 6 - /";
+        System.out.println("PilhaVetor = " + Vetor(exp1) + " " + Vetor(exp2) + "  " + Vetor(exp3) + "  " + Vetor(exp4)/*+Vetor(exp4)*/);
+        System.out.println("PilhaLista = " + Lista(exp1) + " " + Lista(exp2) + "  " + Lista(exp3) + "  " + Lista(exp4)/*+Lista(exp4)*/);
+    }
 
 }
