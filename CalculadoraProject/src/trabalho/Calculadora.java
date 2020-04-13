6f704b923c84507509681c7ac91678fbc90d62c9
Gabriel Andrade dos Santos <gabrielusa11@hotmail.com>
2020-04-12 21:50:00 -0300

    Adição de método e main para teste

--- CalculadoraProject/src/trabalho/Calculadora.java
+++ CalculadoraProject/src/trabalho/Calculadora.java
@@ -1,90 +1,18 @@
 package trabalho;
 
-import Fila.Fila;
-import Fila.FilaVetor;
-import java.util.Arrays;
 import pilha.Pilha;
 import pilha.PilhaVetor;
-
+import pilha.PilhaLista;
 
 public class Calculadora {
 
-    private void inserir(String inserir, Fila<String> fila, byte tipo, Integer aberturas) {
-        switch (tipo) {
-            case 1:
-                try {
-                Double.parseDouble(inserir);
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("Expressão inválida");
-            }
-            break;
-            case -1:
-                return;
-            case 2:
-                if (inserir.trim().length() != 1) {
-                    throw new RuntimeException("Expressão inválida");
-                }
-                break;
-            case 3:
-                if (inserir.contains("(") && inserir.contains(")")) {
-                    throw new RuntimeException("Expressão inválida");
-                } else if (inserir.contains("(")) {
-                    aberturas += inserir.replace(" ", "").length();
-                } else {
-                    aberturas -= inserir.replace(" ", "").length();
-                }
-
-        }
-
-        fila.inserir(inserir.replace(" ", ""));
-    }
-
-    public Fila<String> extrairTermos(String expresao) {
-        byte tipo = -1;
-        Integer aberturas = 0;
-        String valor = "";
-        Fila<String> termos = new FilaVetor<>(expresao.length());
-        for (int i = 0; i < expresao.length(); i++) {
-            if (expresao.charAt(i) >= '0' && expresao.charAt(i) <= '9' && (tipo != 1)) {
-                inserir(valor, termos, tipo, aberturas);
-                valor = "";
-                tipo = 1;
-            } else if (Arrays.asList('+', '-', '*', '/').contains(expresao.charAt(i)) && tipo != 2) {
-                inserir(valor, termos, tipo, aberturas);
-                valor = "";
-                tipo = 2;
-            } else if (expresao.charAt(i) == '(' || expresao.charAt(i) == ')' && tipo != 3) {
-                inserir(valor, termos, tipo, aberturas);
-                valor = "";
-                if (tipo == 1) {
-                    inserir("*", termos, (byte) 2, aberturas);
-                }
-                tipo = 3;
-            }
-            valor += expresao.charAt(i);
-        }
-        inserir(valor, termos, tipo, aberturas);
-        System.out.println(termos.toString());
-        if (aberturas != 0) {
-            throw new RuntimeException("Expressão inválida");
-        }
-        return termos;
-    }
-
-    public Fila<String> gerarExprInfixada(Fila<String> exprInfixada) {
-        return null;
-    }
-
-    public double gerarExprPosfixada(Fila<String> exprPosfixada) {
-        return 0;
-    }
-
-    public static double CalcPilhaVetor(String exp) throws Exception {
+    public static Double Vetor(String exp) throws Exception {
 
         char[] tokens = exp.toCharArray();
 
         // Cria uma pilha vetor vazia
-        Pilha<Double> stack = new PilhaVetor<>(exp.length());
+        Pilha<Double> pilha = new PilhaVetor<>(exp.length());
+        // Pilha<Integer> stack = new PilhaLista<>(exp.length());
 
         for (int i = 0; i < tokens.length; i++) {
 
@@ -99,7 +27,59 @@ public class Calculadora {
                 while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.') {
                     sbud.append(tokens[i++]);
                 }
-                
+                pilha.push(Double.parseDouble(sbud.toString()));
+            } // Se for um operador
+            else {
+
+                // Tirar dois valores
+                double x = pilha.pop();
+                double y = pilha.pop();
+
+                // Faz a operação, e adiciona na pilhaVetor
+                switch (exp.charAt(i)) {
+                    case '+':
+                        pilha.push(y + x);
+                        break;
+                    case '-':
+                        pilha.push(y - x);
+                        break;
+                    case '*':
+                        pilha.push(y * x);
+                        break;
+                    case '/':
+                        pilha.push(y / x);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // resultado da expressão
+        return pilha.pop();
+    }
+
+    public static Double Lista(String exp) throws Exception {
+
+        char[] tokens = exp.toCharArray();
+
+        // Cria uma pilha lista vazia
+        Pilha<Double> stack = new PilhaLista<>();
+        // Pilha<Integer> stack = new PilhaLista<>(exp.length());
+
+        for (int i = 0; i < tokens.length; i++) {
+
+            if (tokens[i] == ' ') {
+                continue;
+            }
+
+            // Se for um número, adiciona na pilhaVetor
+            if (tokens[i] >= '0' && tokens[i] <= '9') {
+                StringBuilder sbud = new StringBuilder();
+                // Se for um número com mais de uma casa
+                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.') {
+                    sbud.append(tokens[i++]);
+                }
                 stack.push(Double.parseDouble(sbud.toString()));
             } // Se for um operador
             else {
@@ -109,19 +89,43 @@ public class Calculadora {
                 double y = stack.pop();
 
                 // Faz a operação, e adiciona na pilhaVetor
-                if (exp.charAt(i) == '+') {
-                    stack.push(y + x);
-                } else if (exp.charAt(i) == '-') {
-                    stack.push(y - x);
-                } else if (exp.charAt(i) == '*') {
-                    stack.push(y * x);
-                } else if (exp.charAt(i) == '/') {
-                    stack.push(y / x);
+                switch (exp.charAt(i)) {
+                    case '+':
+                        stack.push(y + x);
+                        break;
+                    case '-':
+                        stack.push(y - x);
+                        break;
+                    case '*':
+                        stack.push(y * x);
+                        break;
+                    case '/':
+                        stack.push(y / x);
+                        break;
+                    default:
+                        break;
                 }
             }
         }
 
         // resultado da expressão
         return stack.pop();
+    }
+
+    public static void main(String[] args) throws Exception {
+
+//		Expressão (1-2)*(4+5) = -9
+        String exp1 = "1 2 - 4 5 + *";
+
+//		Expressão ((23+12)/7)*(5+(3-12)) = -20
+        String exp2 = "23 12 + 7 / 3 12 - 5 + *";
+
+//		Expressão  5 + (1 + 2) × 4 – 3 = 14
+        String exp3 = "5 1 2 + 4 * + 3 -";
+
+//		Expressão (5.0 + 3) * 2 / (4 - 6) = -8.0    // HELP !!!!!!!!!!!
+        String exp4 = "5.0 3 + 2 * 4 6 - /";
+        System.out.println("PilhaVetor = " + Vetor(exp1) + " " + Vetor(exp2) + "  " + Vetor(exp3) + "  " + Vetor(exp4)/*+Vetor(exp4)*/);
+        System.out.println("PilhaLista = " + Lista(exp1) + " " + Lista(exp2) + "  " + Lista(exp3) + "  " + Lista(exp4)/*+Lista(exp4)*/);
     }
 }
